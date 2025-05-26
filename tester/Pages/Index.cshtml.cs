ce4c2a99cf6885ebf26aa18d5c64ba438dcffad3
lukasprogram <lukas.je@post.cz>
2025-05-26 21:47:33 +0200

    fixed shadow after null task

--- tester/Pages/Index.cshtml.cs
+++ tester/Pages/Index.cshtml.cs
@@ -31,21 +31,23 @@ namespace tester.Pages
         }
 
         public void OnPost() {
+            if (itemList.Count == 0)
+            {
+                var taskJsons = HttpContext.Session.GetString(TodoSessionKey);
+
+                if (!string.IsNullOrEmpty(taskJsons))
+                {
+                    itemList = JsonSerializer.Deserialize<List<string>>(taskJsons);
+                }
+            }
+
 
             if (!string.IsNullOrEmpty(newTask))
             {
 
                 // If tasks already exist in session, load the current tasks
 
-                if (itemList.Count == 0)
-                {
-                    var taskJsons = HttpContext.Session.GetString(TodoSessionKey);
-
-                    if (!string.IsNullOrEmpty(taskJsons))
-                    {
-                        itemList = JsonSerializer.Deserialize<List<string>>(taskJsons);
-                    }
-                }
+                
 
                 // Add new task to the list
                 itemList.Add(newTask);
@@ -53,8 +55,9 @@ namespace tester.Pages
                 // Serialize the updated list and save it back to session
                 var taskJson = JsonSerializer.Serialize(itemList);
                 HttpContext.Session.SetString(TodoSessionKey, taskJson);
-
             }
+
+            
         }
         
     }
